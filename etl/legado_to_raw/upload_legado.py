a8419ba8367fb9c4f4adde1ac69bfc5e5c238f4d
MSc. Eng. Rodrigo Santos <60820402+diggovr@users.noreply.github.com>
2026-01-03 14:44:54 -0300

    Use AWS profile for S3 upload session

--- etl/legado_to_raw/upload_legado.py
+++ etl/legado_to_raw/upload_legado.py
@@ -9,6 +9,7 @@ BUCKET_NAME = "pos-graduacao-lakehouse"
 S3_PREFIX = "legado"
 LOCAL_BASE_PATH = r"C:\Users\Administrator\Desktop\Lakehouse"
 FILE_NAME = "Base_Dados.xlsx"
+AWS_PROFILE = "etl_engenharia_dados"
 
 LOCAL_FILE_PATH = os.path.join(LOCAL_BASE_PATH, FILE_NAME)
 S3_KEY = f"{S3_PREFIX}/{FILE_NAME}"
@@ -17,7 +18,8 @@ S3_KEY = f"{S3_PREFIX}/{FILE_NAME}"
 # FUNÇÕES
 # =========================
 def upload_file_to_s3(local_path, bucket, s3_key):
-    s3_client = boto3.client("s3")
+    session = boto3.Session(profile_name=AWS_PROFILE)
+    s3_client = session.client("s3")
 
     try:
         print(f"[INFO] Iniciando upload do arquivo:")
@@ -35,7 +37,6 @@ def upload_file_to_s3(local_path, bucket, s3_key):
     except ClientError as e:
         print("[ERROR] Falha ao enviar o arquivo para o S3.")
         raise e
-
 
 # =========================
 # EXECUÇÃO
